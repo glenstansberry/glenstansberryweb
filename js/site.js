8e3ba75dd57406677458db89a1afb64d5dee58f2
Glen Stansberry <gstansberry@ljworld.com>
2017-08-06 16:16:07 -0500

    media

---
+++ js/site.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	(function($) {
+		$('#menu_icon').click(function(e){
+			e.preventDefault();
+			$('body').toggleClass('with_sidebar');
+		});    
+    $('#site_cache').click(function(e){
+      $('body').removeClass('with_sidebar');
+    });
+	})(jQuery);
+});
+
+
+
+
